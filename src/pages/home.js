724d31fd897a7ee4b99ba96a82ed8e49e779f543
Jimeneab <abraham.tal.ji@gmail.com>
2021-03-05 12:39:30 -0600

    cards in home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,16 +2,45 @@ import React from 'react'
 import YellowButton from './../components/yellowButton'
 import Grid from '@material-ui/core/Grid';
 import SearchField from './../components/searchField'
+import MediaCard from '../components/card';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    }}))
 
 function Home() {
+  const classes = useStyles();
   return (
     <>
-    <h1>Esta es la pagina de home</h1>
+    <Grid item>
+      <h1>Esta es la pagina de home</h1>
+    </Grid>
     <Grid item>
       <SearchField/>
     </Grid>
     <Grid item>
       <YellowButton text="Post" to='/post'/>
+    </Grid>
+    <Grid container>
+      <Grid item>
+        <MediaCard/>
+      </Grid>
+      <Grid item>
+        <MediaCard/>
+      </Grid>
+      <Grid item>
+        <MediaCard/>
+      </Grid>
+      <Grid item>
+        <MediaCard/>
+      </Grid>
     </Grid>
     </>
   );
