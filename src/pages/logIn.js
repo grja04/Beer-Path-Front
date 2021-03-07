6757db29afdbe734ca4b5f22407088d6b8f81cef
Jimeneab <abraham.tal.ji@gmail.com>
2021-03-06 20:44:14 -0600

    merged with crismmo repo

--- src/pages/logIn.js
+++ src/pages/logIn.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import react from "react"
+import { Avatar, FormControlLabel, Grid, Paper, TextField, Checkbox, Button, Typography, Link } from "@material-ui/core";
+import LockIcon from '@material-ui/icons/Lock';
 
-function LogIn() {
-  return (
-    <>
-    <h1 className="bg-success">Esta es la pagina de logIn</h1>
-    </>
-  );
+const LogIn = () => {
+
+
+    const paperStyle = { padding: 20, height: '70vh', width: 300, margin: "20px auto", backgroundColor: '#FFBF00' }
+    const avatarStyle = { backgroundColor: '#263238' }
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockIcon /></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required />
+                <TextField label='Password' placeholder='Enter passwowrd' type='password' fullWidth required />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            // checked={state.checkedF}
+                            // onChange={handleChange}
+                            name="checkedF"
+                            indeterminate
+                        />
+                    }
+                    label="Remember Me"
+                />
+                <Button type='submit' color='primary' fullWidth variant="contained" style={avatarStyle}>Sign In</Button>
+                <Typography fullWidth> Do you have an account?
+                
+                <Link href="#" /*onClick={preventDefault}*/>
+                 Sign up!
+              </Link>
+              </Typography>
+              <Typography> 
+              <Link href="#" /*onClick={preventDefault}*/>
+              Forgot password?
+            </Link>
+            </Typography>
+            </Paper>
+        </Grid>
+    )
 }
-
-export default LogIn;
+export default LogIn; 
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import react from "react"
+import { Avatar, Grid, Paper, TextField, Button, Typography, Link } from "@material-ui/core";
+import LockIcon from '@material-ui/icons/Lock';
 
-function SignUp() {
-  return (
-    <>
-    <h1 className="bg-success">Esta es la pagina de sign upppp</h1>
-    </>
-  );
+const Signup = () => {
+
+
+    const paperStyle = { padding: 20, height: '70vh', width: 300, margin: "30px auto", backgroundColor: '#FFBF00' }
+    const avatarStyle = { backgroundColor: '#263238' }
+    const buttonStyle = {backgroundColor:'#263238'}
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LockIcon /></Avatar>
+                    <h2>Sign Up</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required />
+                <TextField label='Email' placeholder='Enter email' fullWidth required />
+                <TextField label='Password' placeholder='Enter passwowrd' type='password' fullWidth required />
+               
+                <Button type='submit' color='primary' fullWidth variant="contained" style={buttonStyle}>Sign Up</Button>
+                <Typography fullWidth> Already have an account?
+                
+                <Link href="#" /*onClick={preventDefault}*/>
+                 Sign in!
+              </Link>
+            
+            </Typography>
+            </Paper>
+        </Grid>
+    )
 }
 
-export default SignUp;
+
+export default Signup; 
