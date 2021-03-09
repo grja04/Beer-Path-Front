990150d07eb4c70e99273e227ed7991f736ee0f7
Jimeneab <abraham.tal.ji@gmail.com>
2021-03-09 12:21:54 -0600

    first sprint for singup page

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,23 +1,28 @@
-import react from "react"
+import React,{useState} from "react"
 import { Avatar, Grid, Paper, TextField, Button, Typography, Link } from "@material-ui/core";
 import LockIcon from '@material-ui/icons/Lock';
 
 const Signup = () => {
-
-
+    const [dataEntry, setDataEntry] = useState({});
     const paperStyle = { padding: 20, height: '70vh', width: 300, margin: "30px auto", backgroundColor: '#FFBF00' }
     const avatarStyle = { backgroundColor: '#263238' }
     const buttonStyle = {backgroundColor:'#263238'}
+    const getEntryData = event=>{
+        let property = event.target.name
+        let value = event.target.value
+
+        setDataEntry({...dataEntry, [property]:value})
+    }
     return (
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                    <Avatar style={avatarStyle}><LockIcon /></Avatar>
+                    <Avatar style={avatarStyle}><LockIcon /></Avatar> 
                     <h2>Sign Up</h2>
                 </Grid>
-                <TextField label='Username' placeholder='Enter username' fullWidth required />
-                <TextField label='Email' placeholder='Enter email' fullWidth required />
-                <TextField label='Password' placeholder='Enter passwowrd' type='password' fullWidth required />
+                <TextField name="username" onChange={getEntryData} label='Username' placeholder='Enter username' fullWidth required />
+                <TextField name="email" onChange={getEntryData} label='Email' placeholder='Enter email' fullWidth required />
+                <TextField name="hash" onChange={getEntryData} label='Password' placeholder='Enter passwowrd' type='password' fullWidth required />
                
                 <Button type='submit' color='primary' fullWidth variant="contained" style={buttonStyle}>Sign Up</Button>
                 <Typography fullWidth> Already have an account?
