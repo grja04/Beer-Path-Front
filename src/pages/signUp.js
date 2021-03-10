e27c8b4a5bdb2980275614faf1b1e4d835211a0f
Jimeneab <abraham.tal.ji@gmail.com>
2021-03-10 11:32:50 -0600

    before connect with endpoints

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -10,8 +10,14 @@ const Signup = () => {
     const getEntryData = event=>{
         let property = event.target.name
         let value = event.target.value
-
         setDataEntry({...dataEntry, [property]:value})
+    }
+    const saveDataEntry = ()=>{
+        console.log('el boton esta funcionando en singup')
+        fetch("https://beerpath.herokuapp.com/auth/signup/",{
+            method: "POST",
+            body: dataEntry
+          })
     }
     return (
         <Grid>
@@ -20,22 +26,53 @@ const Signup = () => {
                     <Avatar style={avatarStyle}><LockIcon /></Avatar> 
                     <h2>Sign Up</h2>
                 </Grid>
-                <TextField name="username" onChange={getEntryData} label='Username' placeholder='Enter username' fullWidth required />
-                <TextField name="email" onChange={getEntryData} label='Email' placeholder='Enter email' fullWidth required />
-                <TextField name="hash" onChange={getEntryData} label='Password' placeholder='Enter passwowrd' type='password' fullWidth required />
+                <TextField
+                // value={!dataEntry?"":dataEntry.name} 
+                name="username" 
+                onChange={getEntryData} 
+                label='Username' 
+                placeholder='Enter username' 
+                fullWidth 
+                required 
+                />
+                <TextField
+                // value={!dataEntry?"":dataEntry.email} 
+                name="email" 
+                onChange={getEntryData} 
+                label='Email' 
+                placeholder='Enter email' 
+                fullWidth 
+                required 
+                />
+                <TextField 
+                // value={!dataEntry?"":dataEntry.hash}
+                name="hash" 
+                onChange={getEntryData} 
+                label='Password' 
+                placeholder='Enter passwowrd' 
+                type='password' 
+                fullWidth 
+                required 
+                />
                
-                <Button type='submit' color='primary' fullWidth variant="contained" style={buttonStyle}>Sign Up</Button>
+                <Button
+                type='submit' 
+                color='primary' 
+                fullWidth 
+                variant="contained" 
+                style={buttonStyle}
+                onClick={saveDataEntry}
+                >
+                    Sign Up
+                </Button>
                 <Typography fullWidth> Already have an account?
                 
-                <Link href="#" /*onClick={preventDefault}*/>
+                <Link href="/logIn" /*onClick={preventDefault}*/>
                  Sign in!
-              </Link>
-            
+                </Link>
             </Typography>
             </Paper>
         </Grid>
     )
 }
-
-
 export default Signup; 
