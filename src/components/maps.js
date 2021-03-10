9fad7490935920d9af44cafde1f18e34e5904c82
Crismoo <mooaguilarcris@gmail.com>
2021-03-10 13:40:54 -0600

    Maps zoom change

--- src/components/maps.js
+++ src/components/maps.js
@@ -45,7 +45,7 @@ function MapsContainer() {
             <h1 class="mapProps"></h1>
             <Search />
             <GoogleMap mapContainerStyle={mapContainerStyle}
-                zoom={18}
+                zoom={15}
                 center={center}
                 onClick={onMapClick}
                 onLoad={onMapLoad}
