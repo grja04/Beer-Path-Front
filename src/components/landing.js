7fd3156c6d0b071efda563f8a3288687c7eccc46
Juan Antonio Gonzalez Rojas <grja04@gmail.com>
2021-03-13 23:30:55 -0600

    act ualización landing

---
+++ src/components/landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+
+
+
+export default function Landing() {
+    <Grid container stlye={{ maxHeight: '100%' }}>
+        <Grid item xs={12} sm={6} style={{ backgroundColor: '#F5F5F5' }}>
+            <img src='/landingtop.jpg' style={{ width: '100%', height: '100%',}} alt="landingtop">
+            </img>
+        </Grid>
+    </Grid>
+    }
+    
