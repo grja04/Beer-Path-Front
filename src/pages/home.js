7cbba96da6d45de258e2869c4c1ec4b7883532b6
Juan Antonio Gonzalez Rojas <grja04@gmail.com>
2021-03-15 14:32:13 -0600

    Actualizacion landing

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,87 +1,98 @@
-import React from 'react'
-import YellowButton from './../components/yellowButton'
-import Grid from '@material-ui/core/Grid';
-import MediaCard from '../components/card';
-import { makeStyles } from '@material-ui/core/styles';
-import MapsContainer from '../components/maps'
-import {GoogleMap,useLoadScript, Marker, InfoWindow} from '@react-google-maps/api'; 
-
-
-
+import React from "react";
+import YellowButton from "./../components/yellowButton";
+import Grid from "@material-ui/core/Grid";
+import MediaCard from "../components/card";
+import { makeStyles } from "@material-ui/core/styles";
+import MapsContainer from "../components/maps";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 const useStyles = makeStyles((theme) => ({
   pageRoot: {
-    background:'#F5F5F5',
-    },
-    contentWrapper:{
-      background:'#F5F5F5',
-      justifyContent: 'center',
-      marginTop:'-10%'
-    },
-    landingMiddle:{
-      background:'#CD7F08',
-      justifyContent:'left',
-      marginTop: '3%',
+    background: "#F5F5F5",
+  },
+  landing:{
+    background:'#292929',
+  },
+  landingMiddle: {
+    background: "#CD7F08",
+    justifyContent: "left",
+  },
+  landingBottom: {
+    background: "#403B3B",
+    justifyContent:'right'
 
-    },
-    landingBottom:{
-      background:'#403B3B',
-      justifyContent:'right',
-      marginTop:'3%'
-    },
-    cardWrapper:{
-      background: '#F5F5F5',
-      marginTop: '5%',
-      justifyContent:'center'
-    },
-    mapContainer:{
-      justifyContent:'center',
-      padding:'5%'
-    }
-  }))
+  }, 
+  imageBottom:{
+  justifyContent:'center'
+  },
+  cardBottom: {
+    background: '#F5F5F5',
+    marginLeft:'-50%',
+    marginTop:'50%',
+    padding:'5%'
+  },
+  cardWrapper: {
+    background: "#F5F5F5",
+    marginTop: "5%",
+    justifyContent: "center",
+  },
+  mapContainer: {
+    justifyContent: "center",
+    padding: "5%",
+  },
+}));
 
 function Home() {
-  
   const classes = useStyles();
   return (
     <Grid container className={classes.pageRoot}>
       <Grid container className={classes.landing}>
-      <img src='/landingtop.jpg' style={{ width: '100%', height: '80%',}} alt="landingtop"/> 
-      </Grid>
-      <Grid container className={classes.contentWrapper}>
-        <Grid item>
-          <Grid container className={classes.landingMiddle} >
-              <Grid item xs={12} sm={6} style={{ backgroundColor: '#FFBF00' }}>
-                  <img src='/landingmiddle.jpg' style={{ width: '100%', height: '100%',}} alt="landingmiddle"/>
-              </Grid>
-          </Grid> 
-          <Grid container className={classes.landingBottom}>
-              <Grid item xs={12} sm={6} style={{ backgroundColor: '#F5F5F5' }}>
-                <img src='/landingbottom.svg' style={{ width: '100%', height: '100%',}} alt="landingbottom"/>
-              </Grid>
-          </Grid>    
+        <Grid>
+          <img src="/landingtop.jpg" style={{ width: "150%", height: "95%" }} alt="landingtop"/>
         </Grid>
-
+      </Grid>
+      <Grid container className={classes.landingMiddle}>  
+        <Grid item xs={12} sm={6} style={{ backgroundColor: "#F5F5F5" }}>
+          <img src="/landingmiddle.jpg" style={{ width: "100%", height: "100%" }} alt="landingmiddle"/>
+        </Grid>
+        <Grid>
+          <Grid container style={{ backgroundColor: "#F5F5F5" }} className={classes.cardBottom}>
+          <h1>Texto de prueba para las cervezas</h1>
+          </Grid>
+        </Grid> 
+      </Grid>
+      <Grid container className={classes.landingBottom}>
+        <Grid>
+          <Grid container style={{ backgroundColor: "#F7A205" }} className={classes.cardBottom}>
+          <h1>Texto de prueba para las cervezas</h1>
+          </Grid>
+        </Grid>
+        <Grid container className={classes.imageBottom} item xs={12} sm={6} style={{ backgroundColor: "#403B3B" }}>
+          <img src="/landingbottom.svg" style={{ width: "70%", height: "100%" }} alt="landingbottom"/>
+        </Grid>
       </Grid>
       <Grid container className={classes.cardWrapper}>
-       <Grid item>
-         <MediaCard/>
-       </Grid>
-       <Grid item>
-         <MediaCard/>
-       </Grid>
-       <Grid item>
-         <MediaCard/>
-       </Grid>
-       <Grid item>
-         <MediaCard/>
-       </Grid>
-      </Grid>  
-
-      <Grid container className={classes.mapContainer} >
-         <MapsContainer/>
+        <Grid item>
+          <MediaCard />
+        </Grid>
+        <Grid item>
+          <MediaCard />
+        </Grid>
+        <Grid item>
+          <MediaCard />
+        </Grid>
+        <Grid item>
+          <MediaCard />
+        </Grid>
       </Grid>
-
+      <Grid container className={classes.mapContainer}>
+        <MapsContainer />
+      </Grid>
     </Grid>
   );
 }
